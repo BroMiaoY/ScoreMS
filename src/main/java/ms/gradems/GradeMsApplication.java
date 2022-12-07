551e86f6dfc98b3e6b16d11e6c28c0ef2ec0df3f
苗苗 <2304653544@qq.com>
2022-12-07 23:56:51 +0800

    first

---
+++ src/main/java/ms/gradems/GradeMsApplication.java
@@ -0,0 +1,13 @@
+package ms.gradems;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradeMsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GradeMsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ms/gradems/GradeMsApplicationTests.java
@@ -0,0 +1,13 @@
+package ms.gradems;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GradeMsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
